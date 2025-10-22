12957f689b0fa18314fe38c2cd72e5e7053d0ed9
Damian Diaz <diazdamian@gmail.com>
2025-10-22 14:46:28 -0300

    mod app.jsx-hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Ofertas from './components/Ofertas';
