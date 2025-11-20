eb6a4bda8b822c5369785d912d6d6060de51a5ce
Damian <diazdamian@gmail.com>
2025-11-20 14:28:51 -0300

    agrego filtro y ordenamiento

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Form } from 'react-bootstrap';
 import ProductCard from './ProductCard';
 import { CartContext } from './CartContext';
 
@@ -7,6 +7,8 @@ const ProductList = ({ category = null }) =>{
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const { agregarAlCarrito } = useContext(CartContext);
+  const [barraDeBusqueda, setBarraDeBusqueda] = useState("");
+  const [ordenPrecio, setOrdenPrecio] = useState("");
 
     useEffect(() => {
       let url = 'https://fakestoreapi.com/products'
@@ -30,15 +32,52 @@ const ProductList = ({ category = null }) =>{
     return <div>Loading...</div>;
   }  
 
+  const filteredProducts=products.filter(product=>
+      product.title.toLowerCase().includes(barraDeBusqueda.toLowerCase()) ||
+      product.description.toLowerCase().includes(barraDeBusqueda.toLowerCase())
+  );
+
+  const sortedProducts = [...filteredProducts].sort((a, b) => {
+    if (ordenPrecio === "asc") return a.price - b.price;
+    if (ordenPrecio === "desc") return b.price - a.price;
+    return 0;
+  });
   
   return (
+  <>
+    <Row className="align-items-end mb-3">
+
+      <Col xs={12} md={6}>
+        <Form.Label>Buscar Productos</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Buscar..."
+          value={barraDeBusqueda}
+          onChange={(e) =>setBarraDeBusqueda(e.target.value)}
+        />
+      </Col>
+
+      {/* Select de ordenamiento */}
+      <Col xs={12} md={4} className="mt-2 mt-md-0">  
+      <Form.Label>Ordenar por precio</Form.Label>  
+        <Form.Select
+          value={ordenPrecio}
+          onChange={(e) => setOrdenPrecio(e.target.value)}
+        >
+          <option value="">Seleccione...</option>
+          <option value="asc">menor a mayor</option>
+          <option value="desc">mayor a menor</option>
+        </Form.Select>
+        </Col>
+    </Row>
     <Row>
-      {products.map((product) => (
+      {sortedProducts.map((product) => (
         <Col md={4} key={product.id} className="mb-4">
           <ProductCard product={product} agregarAlCarrito={agregarAlCarrito} />
         </Col>
       ))}
     </Row>
+  </>
   );
 };
 
